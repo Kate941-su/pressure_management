da969631650bf84966716f67983b3cf22ca69ce5
Kaito Kitaya <kkitaya@nzigen.com>
2023-07-08 08:42:09 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'Instagram Clone',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+      ),
+    );
+  }
+}
